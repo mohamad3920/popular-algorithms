94a5e4098e6b12ee882eb4533628c6a0abbf93a4
mohammad mirzadeh <mohammad.mirzadeh@snapppay.ir>
2024-03-14 10:26:35 +0330

    Initial commit

---
+++ src/main/java/com/me/popularalgorithms/PopularAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.me.popularalgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularAlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopularAlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/popularalgorithms/PopularAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.popularalgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
